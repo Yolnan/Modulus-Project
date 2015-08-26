098bae28662c971b9716f3915bdce6da0950337b
Yolnan <yolnan.chen@MU-C1-15.mullen.pvt.k12.co.us>
2015-08-26 09:47:06 -0600

    Initial Commit

---
+++ Modulus Project/src/Modulusproject.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Modulusproject
+	{
+
+		public static void main(String[] args)
+			{
+				int x;
+				int y;
+				int z;
+				int i;
+				Scanner input = new Scanner(System.in);
+				Random num = new Random();
+				
+				//Get user input, how many questions
+				System.out.println("How many modulus questions do you want to solve?");
+				
+				z = input.nextInt();
+				for(i = 0; i <= z; i++) {
+					
+				}
+			}
+
+	}
