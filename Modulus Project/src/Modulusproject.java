00bade24a00f521c9f6194c1916ca6d727745b54
Yolnan <yolnantiger@gmail.com>
2015-08-26 23:03:54 -0600

    Final Version

--- Modulus Project/src/Modulusproject.java
+++ Modulus Project/src/Modulusproject.java
@@ -7,18 +7,83 @@ public class Modulusproject
 			{
 				int x;
 				int y;
-				int z;
-				int i;
+				int Qnum;
+				int MyAns;
+				int UserAns;
+				int count = 0;
+				int correctNum = 0;
 				Scanner input = new Scanner(System.in);
 				Random num = new Random();
 				
-				//Get user input, how many questions
-				System.out.println("How many modulus questions do you want to solve?");
-				
-				z = input.nextInt();
-				for(i = 0; i <= z; i++) {
+				//Ask for number of questions				
+				while(true) {
+					System.out.println("How many modulus questions to do you want to solve? (minimum of 3 questions)");
+					Qnum = input.nextInt();
+					if (Qnum >= 3) {
+						break;
+					} else {
+						System.out.println("You must answer a minimum of 3 questions");
+					}
+				}
+				while(true) {
+					//Generate Question y%x 
+					x = num.nextInt(10) + 1;
+					y = num.nextInt(1000)+ 11;
+					MyAns = y%x;
+					System.out.println("What is the answer to " + y + "%" + x + "?");
+					UserAns = input.nextInt();
 					
+					//Check Answer y%x
+					if(MyAns == UserAns) {
+						System.out.println("Correct");
+						correctNum++;
+					} else {
+						System.out.println("Incorrect.  Correct Answer = " + MyAns);
+					}
+					count++;
+					if(count >= Qnum) {
+						break;
+					}
+					
+					//Generate Question x%y
+					MyAns = x%y;
+					System.out.println("What is the answer to " + x + "%" + y + "?");
+					UserAns = input.nextInt();
+					
+					//Check Answer x%y
+					if(MyAns == UserAns) {
+						System.out.println("Correct");
+						correctNum++;
+					} else {
+						System.out.println("Incorrect.  Correct Answer = " + MyAns);
+					}
+					count++;
+					if(count >= Qnum) {
+						break;
+					}
+					//Generate Question y%y
+					MyAns = y%y;
+					System.out.println("What is the answer to " + y + "%" + y + "?");
+					UserAns = input.nextInt();
+					
+					//Check Answer y%y
+					if(MyAns == UserAns) {
+						System.out.println("Correct");
+						correctNum++;
+					} else {
+						System.out.println("Incorrect.  Correct Answer = " + MyAns);
+					}
+					count++;
+					if(count >= Qnum) {
+						break;
+					}
+					System.out.println("Qnum = " + Qnum + " count = " + count);
+				}
+				//Score
+				if(correctNum == 1) {
+					System.out.println("You answered " + correctNum + " question correct out of " + Qnum);
+				} else {
+					System.out.println("You answered " + correctNum + " questions correct out of " + Qnum);
 				}
 			}
-
-	}
+	}	
